e6fb8aea01e0dbb508401d3bcffb730ca665b08d
Kamran Khan <kamrankhanwxr@gmail.com>
2024-11-14 09:24:20 +0500

    navbar create

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import Link from 'next/link';
+import React from 'react';
+import { BiSearch } from 'react-icons/bi';
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <div>
+      <nav className="bg-gray-800 p-4">
+        <div className="container mx-auto flex items-center justify-between">
+          {/* Search Bar with Icon */}
+       <div className='flex gap-4 w-1/2'>
+       <div className="flex items-center bg-gray-700 rounded-md p-2 max-w-xs w-full">
+            <BiSearch className="text-gray-400 mr-2" size={20} />
+            <input
+              type="text"
+              placeholder="Search..."
+              className="flex-1 bg-gray-700 text-white focus:outline-none"
+            />
+          </div>
 
-export default Navbar
+          {/* Search Button */}
+          <button className="ml-4 border border-green-500 text-green-500 px-3 py-1 rounded-md hover:bg-green-500 hover:text-white transition-colors">
+            Search
+          </button>
+       </div>
+
+          {/* Navigation Links */}
+          <div className="flex space-x-4 ml-4">
+            <Link className="text-white hover:text-blue-400" href="/javascript">
+              JavaScript
+            </Link>
+            <Link className="text-white hover:text-blue-400" href="/react">
+              React
+            </Link>
+            <Link className="text-white hover:text-blue-400" href="/nodejs">
+              Node.js
+            </Link>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
